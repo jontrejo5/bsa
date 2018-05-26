88dfa6076ce33ee9060ce5d5d8d1c991cb92e3eb
jontrejo5 <jtrejo0515@gmail.com>
2018-05-25 21:12:17 -0700

    Working server

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ var requests = require('./routes/request');
 var app = express();
 
 //view engine setup
+app.engine('pug', require('pug').__express)
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
